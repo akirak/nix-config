3835664deec7548705b4280bb78ef63ca2ddd26d
Akira Komamura <akira.komamura@gmail.com>
2025-08-12 20:07:23 +0900

    nixos: Update r8168

--- nixos/profiles/networking/r8168.nix
+++ nixos/profiles/networking/r8168.nix
@@ -10,12 +10,12 @@
   boot.extraModulePackages = [
     (config.boot.kernelPackages.r8168.overrideAttrs (
       _: super: rec {
-        version = "8.054.00";
+        version = "8.055.00";
         src = pkgs.fetchFromGitHub {
           owner = "mtorromeo";
           repo = "r8168";
           rev = version;
-          sha256 = "sha256-KyycAe+NBmyDDH/XkAM4PpGvXI5J1CuMW4VuHcOm0UQ=";
+          sha256 = "0n3gqy68msfhaq5bgndvgdiijgwgvypijfc7d6bbima577xbigm8";
         };
         meta = super.meta // {
           broken = false;
