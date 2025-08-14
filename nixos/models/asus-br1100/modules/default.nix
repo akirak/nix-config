00f7ab3b759b0da90a35948c68a4e449b0083ea7
Akira Komamura <akira.komamura@gmail.com>
2025-08-15 02:08:04 +0900

    nixos/models/asus-br1100: Fix the import path

--- nixos/models/asus-br1100/modules/default.nix
+++ nixos/models/asus-br1100/modules/default.nix
@@ -2,7 +2,7 @@
   imports = [
     (nixos-hardware.outPath + "/common/pc/laptop")
     (nixos-hardware.outPath + "/common/gpu/intel")
-    ../../profiles/networking/r8168.nix
+    ../../../profiles/networking/r8168.nix
     ./wireless.nix
     ./acpi_call.nix
   ];
