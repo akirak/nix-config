293f18d79cb0f48453e32a62f28642a1f5e37b98
Akira Komamura <akira.komamura@gmail.com>
2025-08-14 19:42:48 +0900

    machines/li: Disable ollama

--- machines/li/default.nix
+++ machines/li/default.nix
@@ -34,8 +34,8 @@ in
     # ../../nixos/profiles/nix/cachix-deploy.nix
     ../../nixos/profiles/postgresql/development.nix
     ../../nixos/profiles/livebook
-    ../../nixos/profiles/ollama
     # ../../nixos/profiles/virtualbox-host
+    # ../../nixos/profiles/ollama
     ../../nixos/profiles/dpt-rp1
     ../../nixos/profiles/podman/rootless-docker.nix
     ../../nixos/profiles/ai-mcp
