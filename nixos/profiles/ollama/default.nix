f65ca7d4238204dcbcc431690c3eec96a7a0a363
Akira Komamura <akira.komamura@gmail.com>
2025-12-12 21:57:23 +0900

    nixos/profiles/ollama: Remove the default models

--- nixos/profiles/ollama/default.nix
+++ nixos/profiles/ollama/default.nix
@@ -1,11 +1,10 @@
 {
   services.ollama = {
     enable = true;
-    loadModels = [
-      "mistral"
-      "gemma3:4b"
-      "phi4-mini"
-    ];
+
+    # loadModels = [
+    # ];
+
     # /var/lib/private should be on a persistent file system
   };
 }
