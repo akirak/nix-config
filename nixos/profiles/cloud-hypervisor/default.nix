163686ff424ff1af67f0000fd07fd78b9b355afb
Akira Komamura <akira.komamura@gmail.com>
2026-01-15 12:45:43 +0900

    nixos/profiles/cloud-hypervisor: Add a new profile

---
+++ nixos/profiles/cloud-hypervisor/default.nix
@@ -0,0 +1,19 @@
+{ homeUser, pkgs, lib, ... }:
+{
+  environment.systemPackages = [
+    pkgs.cloud-hypervisor
+  ];
+
+  # Required if you want to enable TAP networks
+  # security.wrappers.cloud-hypervisor = {
+  #   source = lib.getExe pkgs.cloud-hypervisor;
+  #   capabilities = "cap_net_admin+ep";
+  #   owner = "root";
+  #   group = "root";
+  # };
+
+  users.users.${homeUser}.extraGroups = [
+    # Needed to create a tap network with microvm.nix
+    "kvm"
+    ];
+}
